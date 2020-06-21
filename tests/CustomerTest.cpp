56aa4f852b4ef1db1f5eb7f303446d129df7362a
Tiffany Bonzon <tiffany.bonzon@heig-vd.ch>
2020-06-21 23:17:05 +0200

    Mocked Movie in CustomerTest

--- tests/CustomerTest.cpp
+++ tests/CustomerTest.cpp
@@ -8,6 +8,36 @@
 #include "MovieStateChildren.h"
 #include "Rental.h"
 
+class MMovieR : public Movie {
+public:
+    MMovieR() : Movie("Karate Kid", new MovieStateRegular()) {};
+    MOCK_METHOD(std::string, getTitle, ());
+    MOCK_METHOD(double, getPrice, ());
+    MOCK_METHOD(unsigned, getMaxRentPeriodBeforePriceIncrease, ());
+    MOCK_METHOD(double, getPriceIncrease, ());
+    MOCK_METHOD(std::string, getMovieType, ());
+};
+
+class MMovieNR : public Movie {
+public:
+    MMovieNR() : Movie("Avengers: Endgame", new MovieStateNewRelease()) {};
+    MOCK_METHOD(std::string, getTitle, ());
+    MOCK_METHOD(double, getPrice, ());
+    MOCK_METHOD(unsigned, getMaxRentPeriodBeforePriceIncrease, ());
+    MOCK_METHOD(double, getPriceIncrease, ());
+    MOCK_METHOD(std::string, getMovieType, ());
+};
+
+class MMovieC : public Movie {
+public:
+    MMovieC() : Movie("Snow White", new MovieStateChildren()) {};
+    MOCK_METHOD(std::string, getTitle, ());
+    MOCK_METHOD(double, getPrice, ());
+    MOCK_METHOD(unsigned, getMaxRentPeriodBeforePriceIncrease, ());
+    MOCK_METHOD(double, getPriceIncrease, ());
+    MOCK_METHOD(std::string, getMovieType, ());
+};
+
 TEST(Customer, name) {
     std::string name = "Jimmy";
     Customer customer(name);
@@ -17,10 +47,32 @@ TEST(Customer, name) {
 
 TEST(Customer, statement) {
 
+    MMovieR movieR;
+    MMovieNR movieNR;
+    MMovieC movieC;
+
+    EXPECT_CALL(movieR, getTitle()).WillRepeatedly(testing::Return("Karate Kid"));
+    EXPECT_CALL(movieR, getPrice()).WillRepeatedly(testing::Return(2));
+    EXPECT_CALL(movieR, getMaxRentPeriodBeforePriceIncrease()).WillRepeatedly(testing::Return(2));
+    EXPECT_CALL(movieR, getPriceIncrease()).WillRepeatedly(testing::Return(1.5));
+    EXPECT_CALL(movieR, getMovieType()).WillRepeatedly(testing::Return("Regular"));
+
+    EXPECT_CALL(movieNR, getTitle()).WillRepeatedly(testing::Return("Avengers: Endgame"));
+    EXPECT_CALL(movieNR, getPrice()).WillRepeatedly(testing::Return(0));
+    EXPECT_CALL(movieNR, getMaxRentPeriodBeforePriceIncrease()).WillRepeatedly(testing::Return(0));
+    EXPECT_CALL(movieNR, getPriceIncrease()).WillRepeatedly(testing::Return(3));
+    EXPECT_CALL(movieNR, getMovieType()).WillRepeatedly(testing::Return("NewRelease"));
+
+    EXPECT_CALL(movieC, getTitle()).WillRepeatedly(testing::Return("Snow White"));
+    EXPECT_CALL(movieC, getPrice()).WillRepeatedly(testing::Return(1.5));
+    EXPECT_CALL(movieC, getMaxRentPeriodBeforePriceIncrease()).WillRepeatedly(testing::Return(3));
+    EXPECT_CALL(movieC, getPriceIncrease()).WillRepeatedly(testing::Return(1.5));
+    EXPECT_CALL(movieC, getMovieType()).WillRepeatedly(testing::Return("Children"));
+
     Customer customer("Olivier");
-    customer.addRental( Rental( Movie("Karate Kid", new MovieStateRegular()), 7));
-    customer.addRental( Rental( Movie( "Avengers: Endgame", new MovieStateNewRelease() ), 5));
-    customer.addRental( Rental( Movie("Snow White", new MovieStateChildren), 3 ));
+    customer.addRental( Rental( movieR, 7));
+    customer.addRental( Rental( movieNR, 5));
+    customer.addRental( Rental( movieC, 3 ));
 
     std::string expected = "Rental Record for Olivier\n"
                            "\tKarate Kid\t9.5\n"
