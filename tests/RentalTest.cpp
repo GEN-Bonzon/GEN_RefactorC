6cc21256790d1ef83aebe4b009389348230f9506
Tiffany Bonzon <tiffany.bonzon@heig-vd.ch>
2020-06-21 22:51:15 +0200

    Mocked Movie in RentalTest

--- tests/RentalTest.cpp
+++ tests/RentalTest.cpp
@@ -3,20 +3,37 @@
 //
 
 #include <gtest/gtest.h>
+#include <gmock/gmock.h>
 
 #include "Rental.h"
-#include "Movie.h"
 #include "MovieStateRegular.h"
 
-Movie movie("Karate Kid", new MovieStateRegular());
+class MMovie : public Movie {
+    public:
+        MMovie() : Movie("Karate Kid", new MovieStateRegular()) {};
+        MOCK_METHOD(std::string, getTitle, ());
+        MOCK_METHOD(double, getPrice, ());
+        MOCK_METHOD(unsigned, getMaxRentPeriodBeforePriceIncrease, ());
+        MOCK_METHOD(double, getPriceIncrease, ());
+        MOCK_METHOD(std::string, getMovieType, ());
+};
+
+MMovie movie;
 Rental rental(movie, 42);
 
+
 TEST(Rental, movie) {
-    ASSERT_TRUE(movie.getTitle() == rental.getMovie().getTitle());
-    ASSERT_TRUE(movie.getPrice() == rental.getMovie().getPrice());
-    ASSERT_TRUE(movie.getMaxRentPeriodBeforePriceIncrease() == rental.getMovie().getMaxRentPeriodBeforePriceIncrease());
-    ASSERT_TRUE(movie.getPriceIncrease() == rental.getMovie().getPriceIncrease());
-    ASSERT_TRUE(movie.getMovieType() == rental.getMovie().getMovieType());
+    EXPECT_CALL(movie, getTitle()).WillRepeatedly(testing::Return("Karate Kid"));
+    EXPECT_CALL(movie, getPrice()).WillRepeatedly(testing::Return(2));
+    EXPECT_CALL(movie, getMaxRentPeriodBeforePriceIncrease()).WillRepeatedly(testing::Return(2));
+    EXPECT_CALL(movie, getPriceIncrease()).WillRepeatedly(testing::Return(1.5));
+    EXPECT_CALL(movie, getMovieType()).WillRepeatedly(testing::Return("Regular"));
+
+    ASSERT_EQ(movie.getTitle(), rental.getMovie().getTitle());
+    ASSERT_EQ(movie.getPrice(), rental.getMovie().getPrice());
+    ASSERT_EQ(movie.getMaxRentPeriodBeforePriceIncrease(), rental.getMovie().getMaxRentPeriodBeforePriceIncrease());
+    ASSERT_EQ(movie.getPriceIncrease(), rental.getMovie().getPriceIncrease());
+    ASSERT_EQ(movie.getMovieType(), rental.getMovie().getMovieType());
 }
 
 TEST(Rental, daysRented) {
